ec96678a03d7a4447d60e345d64a272acb801d34
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2023-03-17 15:37:46 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
+import { useState, useEffect } from 'react';
+
+import { nanoid } from 'nanoid';
+
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './GlobalStyle/Layout/Layout.styled';
+
+import { Section } from './Section';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+
+import data from '../data/contacts';
+
 export const App = () => {
+  const localContacts = JSON.parse(localStorage.getItem('contacts'));
+  const initialContacts = data.map(contact => {
+    contact.id = nanoid();
+    return contact;
+  });
+
+  const [contacts, setContacts] = useState(
+    () => localContacts ?? initialContacts
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    const currentName = contacts.find(item => item.name === name);
+
+    if (currentName && name === currentName.name) {
+      alert(`${name} is already exist!`);
+      return;
+    }
+
+    setContacts(prevState => [{ name, number, id: nanoid() }, ...prevState]);
+  };
+
+  const removeContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getFilteredContacts = () => {
+    const normalizedCaseContacts = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedCaseContacts)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title="Add contact">
+        <ContactForm onSubmit={addContact} />
+      </Section>
+      {contacts.length > 0 && (
+        <Section
+          title="Contacts"
+          headerContent={<Filter value={filter} onChange={changeFilter} />}
+        >
+          <ContactList contacts={filteredContacts} onRemove={removeContact} />
+        </Section>
+      )}
+
+      <GlobalStyle />
+    </Layout>
   );
 };
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,63 @@
+import PropTypes from 'prop-types';
+import { IoIosTrash, IoLogoReddit } from 'react-icons/io';
+
+import {
+  ListBtn,
+  ContactsTable,
+  ContactsTableHead,
+  ContactsTableRow,
+  ContactsTableCeil,
+  ContactsFlexCeil,
+} from './ContactList.styled';
+
+export const ContactList = ({ contacts, onRemove }) => {
+  return (
+    <>
+      <ContactsTable>
+        <thead>
+          <tr>
+            <ContactsTableHead>Name</ContactsTableHead>
+            <ContactsTableHead>Phone number</ContactsTableHead>
+            <ContactsTableHead>Contacts ({contacts.length})</ContactsTableHead>
+          </tr>
+        </thead>
+
+        <tbody>
+          {contacts.map(({ id, name, number }) => {
+            return (
+              <ContactsTableRow key={id}>
+                <ContactsFlexCeil>
+                  {' '}
+                  <IoLogoReddit size="20" />
+                  {name}
+                </ContactsFlexCeil>
+                <ContactsTableCeil>{number}</ContactsTableCeil>
+                <ContactsTableCeil>
+                  <ListBtn
+                    type="button"
+                    onClick={() => {
+                      onRemove(id);
+                    }}
+                  >
+                    <IoIosTrash size="20" />
+                  </ListBtn>
+                </ContactsTableCeil>
+              </ContactsTableRow>
+            );
+          })}
+        </tbody>
+      </ContactsTable>
+    </>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
